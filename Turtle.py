e9c23d663b76918dafad8bab133b83835735ae91
RedstonedLife <37072229+RedstonedLife@users.noreply.github.com>
2018-05-25 10:19:43 +0300

    Create Turtle.py

---
+++ Turtle.py
@@ -0,0 +1,12 @@
+
+#Imports
+import turtle
+print("Please input a Degree (Ex: 90)")
+degree = input()
+print("Please input amount of runs (Ex: 55)")
+am = input()
+
+#Turtle variables
+t = turtle.Turtle()
+
+#Input variables
